1b764ca91ccd1969653f760c80997e3ffa8cbebb
iresha223 <ireshagayanisen223@gmail.com>
2022-02-27 21:58:37 -0500

    updated api

--- AddUpdate_review.php
+++ AddUpdate_review.php
@@ -16,28 +16,42 @@ if($_POST)
     // prepare and bind
     
     $stmt = null;
-
+    $data = [];
 
     if($id > 0)
     {
         $stmt = $conn->prepare("UPDATE review SET comment = ? WHERE id = ?");
         $stmt->bind_param('sd',$comment, $id);
+
+        $data['code'] = 200;
+        $data['message'] = "Review updated successfully!";
+
+        echo json_encode($data);
         
     }
     else
     {
         $stmt = $conn->prepare("INSERT INTO review(product_id,customer_name,comment,rating) VALUES (?, ?, ?, ?)");
         $stmt->bind_param('dssd',$productId, $name, $comment, $rating);
+
+        $data['code'] = 200;
+        $data['message'] = "Review inserted successfully!";
+
+        echo json_encode($data);
     }
 
     $stmt->execute();
 
 }
 
+else{
+
+    $data['code'] = 500;
+    $data['message'] = "Error!";
+
+    echo json_encode($data);
+}
 
 $conn->close();
 
-
-
-echo $_POST['id'];
 ?>
